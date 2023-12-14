3bfb5ac3caa2ccc3fe320137e191bfef5f21c01b
Ronald Abimbola <r.abimbola@alustudent.com>
2023-12-14 06:33:12 -0800

    Task

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Puppet Script
+exec { 'fix-issue':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ web_stack_debugging_4/README.md
@@ -0,0 +1,3 @@
+# Web Stack Debugging #4
+
+![frdkCrb](https://github.com/Ronnie5562/alu-system_engineering-devops/assets/110787129/e9fee759-5241-4f1c-b231-602f1a482ddf)
